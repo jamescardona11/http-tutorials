3b8d00e0ad56179abf500c623f814a273fadcadd
jamescardona11 <jjcardonao@gmail.com>
2020-07-29 12:53:15 -0500

    create a chopper with interceptors

--- chopper_one_app/lib/main.dart
+++ chopper_one_app/lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:chopper_one_app/ui/movie_listings.dart';
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
 
 void main() {
+  _setupLogging();
   runApp(MyApp());
+}
+
+void _setupLogging() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
 }
 
 class MyApp extends StatelessWidget {
