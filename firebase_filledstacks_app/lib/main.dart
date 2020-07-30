f677d7ccdf74b094df3f14452db648dd038fe724
jamescardona11 <jjcardonao@gmail.com>
2020-07-30 14:19:42 -0500

    Init project from: https://www.filledstacks.com/post/firebase-authentication-in-flutter/

--- firebase_filledstacks_app/lib/main.dart
+++ firebase_filledstacks_app/lib/main.dart
@@ -1,6 +1,16 @@
+import 'package:firebase_filledstacks_app/ui/views/signup_view.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_filledstacks_app/services/navigation_service.dart';
+import 'package:firebase_filledstacks_app/services/dialog_service.dart';
+import 'package:firebase_filledstacks_app/ui/views/login_view.dart';
+import 'managers/dialog_manager.dart';
+import 'ui/router.dart';
+import 'locator.dart';
 
 void main() {
+  // Register all the models and services before the app starts
+  setupLocator();
+
   runApp(MyApp());
 }
 
@@ -8,17 +18,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+      title: 'Compound',
+      builder: (context, child) => Navigator(
+        key: locator<DialogService>().dialogNavigationKey,
+        onGenerateRoute: (settings) => MaterialPageRoute(builder: (context) => DialogManager(child: child)),
       ),
+      navigatorKey: locator<NavigationService>().navigationKey,
+      theme: ThemeData(
+        primaryColor: Color.fromARGB(255, 9, 202, 172),
+        backgroundColor: Color.fromARGB(255, 26, 27, 30),
+        textTheme: Theme.of(context).textTheme.apply(
+              fontFamily: 'Open Sans',
+            ),
+      ),
+      home: SignUpView(),
+      onGenerateRoute: generateRoute,
     );
   }
 }
